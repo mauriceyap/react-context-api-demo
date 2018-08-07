674dab81867d31bfed70d61230a3e7b07fafe148
Maurice Yap <mauriceyap@hotmail.co.uk>
2018-08-07 11:40:02 +0100

    initialise stock selector with AAPL

--- src/components/StockSelector.js
+++ src/components/StockSelector.js
@@ -8,7 +8,7 @@ export default class StockSelector extends Component {
     constructor() {
         super();
         this.state = {
-            selectedSecurity: null,
+            selectedSecurity: 'AAPL',
         };
         this.setSelectedSecurity = this.setSelectedSecurity.bind(this);
     }
